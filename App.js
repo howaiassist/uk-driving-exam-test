dba7245a97aff6df677b15809ca724586f6b1af3
howaiassist <howaiassist@gmail.com>
2025-09-29 04:50:54 +0800

    Add .env and update project files for EAS Build

---
+++ App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>UK Driving Exam Test</Text>
+    </View>
+  );
+}
