4419f3e72cf0ef6ea76875a6c125d339663a8d1c
howaiassist <howaiassist@gmail.com>
2025-09-29 07:08:51 +0800

    Fix native module list error for Expo SDK 54

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['module:@react-native/babel-preset'],
     plugins: [
-      'react-native-reanimated/plugin', // Correct plugin path
+      'react-native-reanimated/plugin',
       ['module:react-native-dotenv', {
         envName: 'APP_ENV',
         moduleName: '@env',
