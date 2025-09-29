651f30611aa5544acd90c861fb7389a508633d8f
howaiassist <howaiassist@gmail.com>
2025-09-30 07:27:28 +0800

    Fix app.json schema and prebuild sync issues

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,6 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', {
-        env: {
-          production: {
-            plugins: ['react-native-paper/babel']
-          }
-        }
-      }],
-      'module:react-native-dotenv'
-    ],
-    plugins: [
-      'react-native-reanimated/plugin',
-      ['module:react-native-dotenv', {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-      }],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
